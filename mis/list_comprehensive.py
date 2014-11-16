9f5e1dec28092004d652632000717c102fc75443
AlanTai <rainman.tai@gmail.com>
2014-11-16 15:51:48 -0800

    add new question string combination by list comprehensive and zip

--- mis/list_comprehensive.py
+++ mis/list_comprehensive.py
@@ -1,6 +1,9 @@
-given_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+given_list = [0, 1, 2, -1, 4, 12, 6, -7, 8, 9, 11, -11, 12, 13, 14, 15, 16, 17, 18, 19]
 result_1 = filter(lambda x: x % 3 == 2, given_list)
 print result_1
 
 result_2 = [x for i, x in enumerate(given_list) if i % 3 == 2]
 print result_2
+
+x = "abcdefghijklm"
+print [ith + jth for ith, jth in zip(x[::2], x[1::2])]
