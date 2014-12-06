4b148f2252c85c6be9be7c561ed3bd89ce2120ce
AlanTai <rainman.tai@gmail.com>
2014-12-05 16:20:49 -0800

    update solution for delete duplicated characters

--- mis/delete_duplicated_characters.py
+++ mis/delete_duplicated_characters.py
@@ -1,5 +1,23 @@
 from collections import OrderedDict
 
+def remove_duplicated_char_in_each_word(arg_txt):
+    char_list = []
+    result = ""
+    for char in arg_txt:
+        if char != " " and char not in char_list:
+            char_list.append(char)
+            result = result + char
+        elif char == " ":
+            result = result + char
+            char_list = []
+    
+    return result
+    
+
 if __name__ == "__main__":
+    ''' remove duplicated characters and only keep the first character'''
+    
     given_str = "Shellless mollusk lives in wallless house in wellness. Aaaarrghh!"
-    print "".join(OrderedDict.fromkeys(given_str))
+    # print "".join(OrderedDict.fromkeys(given_str))
+    
+    print remove_duplicated_char_in_each_word(given_str)
