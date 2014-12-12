4acf21ec9cc77d73dba6c2c362484387b530b35e
AlanTai <rainman.tai@gmail.com>
2014-12-11 16:44:04 -0800

    update solution for the question, longest common subsequence

--- CodeEvl/find_longest_common_subsequence.py
+++ CodeEvl/find_longest_common_subsequence.py
@@ -33,23 +33,46 @@ def solution(arg_str):
                     result_matrix[index1].append(max_val)
                     
     max_length = max(result_matrix[len(result_matrix) - 1])
+    return result_matrix
                     
-    return max_length
+def trackback(arg_matrix):
+    result = {}
+    for elem in arg_matrix:
+        max_val = max(elem)
+        max_val_index = elem.index(max_val)
+        if max_val not in result:
+            result[max_val] = max_val_index
+        else:
+            if result[max_val] > max_val_index:
+                result[max_val] = max_val_index
+    return result
                     
     
     
 if __name__ == "__main__":
     given_str = "XMJYAUZ;MZJAWXU"
-    
+
     list_str = given_str.split(";")
     str1 = list_str[0]
     str2 = list_str[1]
-    print difflib.SequenceMatcher(None, str1, str2).get_matching_blocks()
-    print solution(given_str)
+    list2 = list(str2)
+    
+    result_matrix = solution(given_str)
+    result_dict = trackback(result_matrix)
+    print "".join(list2[result_dict[key]] for key in result_dict)
+    
+    # print difflib.SequenceMatcher(None, str1, str2).get_matching_blocks()
     
     
     
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    # 'test' represents the test case, do something with it
+    print test
     
+test_cases.close()
     
     
     
