4258772b5f1c968042177a2b657037d0b516f3ae
AlanTai <rainman.tai@gmail.com>
2014-12-19 09:40:13 -0800

    add new question and solution

---
+++ mis/find_duplicated_number.py
@@ -0,0 +1,16 @@
+def solution(arg_list):
+    dict_result = {}
+    list_duplicated_numbers = []
+    for elem in arg_list:
+        if elem not in dict_result:
+            dict_result[elem] = 1
+        else:
+            dict_result[elem] = dict_result[elem] + 1
+            if elem not in list_duplicated_numbers:
+                list_duplicated_numbers.append(elem)
+            
+    return list_duplicated_numbers
+    
+if __name__ == "__main__":
+    given_list = [1,2,3,2,4,5,5,7,76,67,34,32,4,6,57,8,7,7,86,2,1,4,86]
+    print solution(given_list)
