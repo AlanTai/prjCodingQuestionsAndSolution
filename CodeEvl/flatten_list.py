b7298d15d440457a8614732ee8f52816af25fa0c
AlanTai <rainman.tai@gmail.com>
2014-12-27 12:38:48 -0800

    add new solution of breadth-first to flatting list

--- CodeEvl/flatten_list.py
+++ CodeEvl/flatten_list.py
@@ -5,11 +5,11 @@ Created on Oct 26, 2014
 '''
 
 # depth-first solution
-def solution(A):
+def solution_dfs(A):
     result = []
     while len(A) > 0:
         if isinstance(A[0], list):
-            result += solution(A[0])
+            result += solution_dfs(A[0])
         else:
             result.append(A[0])
         
@@ -17,8 +17,33 @@ def solution(A):
     return result
     
 # bread-first solution
+def solution_bfs(arg_list):
+    list_of_list = []
+    
+    while len(arg_list) > 0:
+        elem = arg_list.pop()
+        if isinstance(elem, list):
+            list_of_list = list_of_list + elem
+        else:
+            yield elem
+            
+        if len(arg_list) == 0:
+            arg_list = list_of_list
+            list_of_list = []
         
         
 if __name__ == "__main__":
     given_list = [1,2,[5,4,3,[2,1,[2,5,4,7,[0]]]],[0,1,[2]],5,[3,4,[1,2,9,0],[4,3,[3,[2,0,0,[1,2],1],2]]],2,[1,2,[4],5,4,[1,2,3]],1,2,9]
-    print solution(given_list)
+    print solution_dfs(given_list)
+    
+    given_list = [1,2,[5,4,3,[2,1,[2,5,4,7,[0]]]],[0,1,[2]],5,[3,4,[1,2,9,0],[4,3,[3,[2,0,0,[1,2],1],2]]],2,[1,2,[4],5,4,[1,2,3]],1,2,9]
+    list_result = []
+    for elem in solution_bfs(given_list):
+        list_result.append(elem)
+        
+    print list_result
+        
+    
+    
+    
+    
